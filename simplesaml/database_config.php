9973aa7d4a44b4d36de46593c26d0bd2d6aa648c
root <root@ServiceProvider.fgu40t1iesiuzgmcp4qkf3vvmd.ax.internal.cloudapp.net>
2021-06-14 03:07:52 +0000

    SP

---
+++ simplesaml/database_config.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'serverName' => 'fgiron1.database.windows.net',
+    'database' => 'Votos',
+    'uid' => 'ElectorLogin',
+    'pwd' => 'fW%qg8&g^PV2VnA43'
+);
+
---
+++ simplesaml/identidades_config.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'serverName' => 'identidades.database.windows.net',
+    'database' => 'Identidades',
+    'uid' => 'SPLogin',
+    'pwd' => '+2uF#e9aY4rT-2RC'
+);
+
